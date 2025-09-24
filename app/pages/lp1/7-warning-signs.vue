4921383cbc0695664e9c5af115cef207cd5ea1db
deepak.kumar <deepak.kumar@suretekinfosoft.com>
2025-09-24 11:43:09 +0530

    ui chnages

--- app/pages/lp1/7-warning-signs.vue
+++ app/pages/lp1/7-warning-signs.vue
@@ -1,3 +1,20 @@
 <template>
-    Hello Deepak
+
+    <!-- top-header -->
+    <div class="bg-black flex justify-center items-center gap-3 py-2">
+        <p class="text-white extrablod">FOR SALE</p>
+        <div class="flex gap-2">
+            <p class="bg-white text-black text-center py-1 px-4  rounded-sm">
+                <span class="block extrablod text-lg">02</span>
+                <span class="font-bold text-sm">HRS</span>
+            </p>
+          
+            
+
+
+        </div>
+
+    </div>
+    <!-- top-header -->
+
 </template>
