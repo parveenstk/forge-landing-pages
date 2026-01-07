7b326952cdb4dfca43ef986cfdee06b510ba8dd7
parveenstk <parveen.kumar@suretekinfosoft.com>
2026-01-07 17:41:25 +0530

    GTM Tag fixed & Meta Pixel Implemented

--- app/components/PopUp/index.vue
+++ app/components/PopUp/index.vue
@@ -5,7 +5,7 @@ const updateStep = (currentStep: number) => {
 }
 </script>
 <template>
-    <PopUpOne v-if="step === 1" :step="step" :update-step="updateStep" />
+    <PopUpOne v-if="step === 1" :step="step" :update-step="updateStep" /> 
     <PopUpTwo v-if="step === 2" :step="step" :update-step="updateStep" />
     <PopUpThree v-if="step === 3" :step="step" :update-step="updateStep" />
     <PopUpFour v-if="step === 4" :step="step" :update-step="updateStep" />
